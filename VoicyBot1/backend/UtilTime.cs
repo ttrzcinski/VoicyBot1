73c46c0d2bf79c1ede262ed7f80005da5f24f8eb
Tomasz Trzcinski <tomasz.trzcinski@capgemini.com>
2019-01-11 13:57:08 +0100

    Added util class for time methods.

---
+++ VoicyBot1/backend/UtilTime.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace VoicyBot1.backend
+{
+    public class UtilTime
+    {
+        /// <summary>
+        /// Returns current timestamp as one continous string yyyyMMddHHmm.
+        /// </summary>
+        /// <returns>now as yyyyMMddHHmm</returns>
+        public string Now() => DateTime.Now.ToString("yyyyMMddHHmm");
+    }
+}
---
+++ VoicyBot1Tests/backend/UtilTimeTests.cs
@@ -0,0 +1,23 @@
+﻿using VoicyBot1.backend;
+using Xunit;
+
+namespace VoicyBot1Tests.backend
+{
+    public class UtilTimeTests
+    {
+        [Fact]
+        public void Now_Test()
+        {
+            // Arrange
+            var utilTime = new UtilTime();
+
+            // Act
+            var result = utilTime.Now();
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.False(string.IsNullOrWhiteSpace(result));
+            Assert.Equal(12, result.Length);
+        }
+    }
+}
